0982a434fade18d631109417edeee507863247b4
GeLiang <childliang@hotmail.com>
2022-09-27 19:54:34 +0800

    约定:Action方法的返回值

---
+++ DotnetCore/WebApi/WebApiConvention.md
@@ -0,0 +1,5 @@
+1. 控制器父类
+   1. Web API 建议 ControllerBase, 除非 webapi control 需要提供 和MVC一样的功能
+2. action方法
+   1. 使用异步,方法名可以不用Async 结尾(因为action是给前端调用的)
+   2. 推荐使用ActionResult<T> , 可以让swagger 知道方法的返回值
---
+++ DotnetCore/WebApi/WebApiConvention/Controllers/ActionConventionController.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApiConvention.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ApiController]
+    [Route("[controller]/[action]")] //推荐使用这个路由
+    public class ActionConventionController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ActionConventionController> _logger;
+
+        public ActionConventionController(ILogger<ActionConventionController> logger)
+        {
+            _logger = logger;
+        }
+
+        private WeatherForecast[] _WeatherForecastArray = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }).ToArray();
+
+
+        #region 约定:Action方法的返回值
+        [HttpGet()]
+        public ActionResult ReturnValue()  //不推荐用 ActionResult 或 IActionResult
+        {
+            return new JsonResult(_WeatherForecastArray);
+        }
+
+        [HttpGet()]
+        public ActionResult<IEnumerable<WeatherForecast>> ReturnValueConvention() //推荐使用 ActionResult<T>
+        {
+            return _WeatherForecastArray;
+        }
+
+        [HttpGet()]
+        public IEnumerable<WeatherForecast> ReturnValueConvention_2() //这个效果和上面一样
+        {
+            return _WeatherForecastArray;
+        }
+
+        [HttpGet()]
+        public ActionResult<int> ReturnValueConvention2() //示例2
+        {
+            return 1;
+        }
+
+        [HttpGet()]
+        public int ReturnValueConvention2_2() //示例2,传统写法
+        {
+            return 1;
+        }
+
+        [HttpGet()]
+        public ActionResult<WeatherForecast> ReturnValueConvention3()//示例3
+        {
+            return _WeatherForecastArray[0];
+        }
+
+        [HttpGet()]
+        public WeatherForecast ReturnValueConvention3_2() //示例3,传统写法
+        {
+            return _WeatherForecastArray[0];
+        }
+        #endregion
+    }
+}
